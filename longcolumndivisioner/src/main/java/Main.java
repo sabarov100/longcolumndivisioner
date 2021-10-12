d40606b8713695e85a7ba8ee2bfc8c2980a7a41f
Evgeny Sabarov <e.sabarov@gmail.com>
2021-10-12 21:25:49 +0300

    add java files: main, solution and test

---
+++ longcolumndivisioner/src/main/java/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+            
+        LongColumnDivisioner longColumnDivisioner = new LongColumnDivisioner();
+        System.out.println(longColumnDivisioner.longColumnDivision(405022500, 45));
+
+    }
+
+}
